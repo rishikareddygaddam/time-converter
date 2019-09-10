c720f8744ab1caca9da8289a7b30c799c3e7173e
Student <student@nw45512.nwmissouri.edu>
2019-09-10 10:53:32 -0500

    second commit

--- Time converter/ViewController.swift
+++ Time converter/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var yearsTF:UITextField!
+    
+    @IBOutlet weak var monthsLBL: UILabel!
+    
+    @IBOutlet weak var daysTF: UILabel!
+    
+    @IBOutlet weak var secondsTF: UILabel!
+    
+    @IBAction func convert(_ sender:UIButton){
+        
+        
+        if let years = Int(yearsTF.text!){
+            let months = years * 12
+            monthsLBL.text = "\(months)"
+        }
+//        else{
+//            let ac = UIAlertController(title: "Missing years", message: "The years text field needs an int value", preferredStyle: .alert)
+//            let action =
+//        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "Time Converter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Convert", style: .plain, target: self, action: #selector(convert))
+        
+        
     }
 
 
